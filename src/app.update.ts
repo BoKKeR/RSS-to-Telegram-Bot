93f81814a29a84f4e30258bf6f3fdd23bf3c1c99
Norber Takacs <bokker11@hotmail.com>
2022-03-18 23:11:33 +0100

    added better validation

--- src/app.update.ts
+++ src/app.update.ts
@@ -1,7 +1,7 @@
 import { Update, Help, Command } from "nestjs-telegraf";
 import { Context } from "./context.interface";
 import { RssService } from "./rss/rss.service";
-import { delay } from "./util/config";
+import { chatid, delay } from "./util/config";
 let Parser = require("rss-parser");
 @Update()
 export class AppUpdate {
@@ -10,7 +10,18 @@ export class AppUpdate {
   @Command("list")
   async startCommand(ctx: Context) {
     const list = await this.rssService.users({});
-    await ctx.reply(JSON.stringify(list));
+
+    if (list.length === 0) {
+      await ctx.reply("The database is empty");
+      return;
+    }
+
+    for (let elementIndex = 0; elementIndex < list.length; elementIndex++) {
+      const entry = list[elementIndex];
+      await ctx.reply(
+        `Title: ${entry.name}\nrss url: ${entry.link}\nlast checked entry: ${entry.last}`
+      );
+    }
   }
 
   @Command("add")
@@ -19,7 +30,9 @@ export class AppUpdate {
     const text = ctx.update.message.text;
 
     if (!text || text.split(" ").length === 2) {
-      await ctx.reply("Error input");
+      await ctx.reply(
+        "ERROR: wrong input, the format needs to be: /add title_name rss_link_url"
+      );
       return;
     }
 
@@ -30,7 +43,7 @@ export class AppUpdate {
 
     if (!link || link === "invalid") {
       await ctx.reply(
-        "Error: something with the link? correct syntax: \n/add link_name rss_link_url"
+        "ERROR: something with the link? correct syntax: \n/add link_name rss_link_url"
       );
       return;
     }
@@ -44,15 +57,12 @@ export class AppUpdate {
         name: name,
         link: link,
       });
-      await ctx.reply(`added link to database as: **${name}** 
-
-Last item link+title:
-link: ${lastItem.link}
-text: ${lastItem.text}
-      `);
+      await ctx.reply(
+        `ADDED: \nRSS: ${lastItem.link}\nTITLE: ${lastItem.text}`
+      );
     } catch (error) {
       if (error.code === "P2002") {
-        await ctx.reply("Error: Duplicate link");
+        await ctx.reply("ERROR: Duplicate link");
       }
     }
   }
@@ -60,24 +70,30 @@ text: ${lastItem.text}
   @Command("remove")
   async onRemove(ctx: Context) {
     // @ts-ignore
-    const entry = ctx.update.message.text.split(" ")[1];
-    if (!entry) {
+    const entries = ctx.update.message.text
+      .replace("/remove", "")
+      .split(" ")[1];
+    if (!entries) {
       await ctx.reply(
-        "Error: wrong input, correct syntax: \n/remove link_name"
+        "ERROR: wrong input, correct syntax: \n/remove link_name link_name link_name"
       );
       return;
     }
     try {
-      await this.rssService.deleteUser({ name: entry });
+      for (let entryIndex = 0; entryIndex < entries.length; entryIndex++) {
+        const element = entries[entryIndex];
+
+        await this.rssService.deleteUser({ name: element });
+        await ctx.reply("REMOVED: " + element);
+      }
     } catch (error) {
       if (error === "P2025") {
-        await ctx.reply("Record not found");
+        await ctx.reply("ERROR: Record not found");
         return;
       }
-      await ctx.reply("Error " + error);
+      await ctx.reply("ERROR " + error);
       return;
     }
-    await ctx.reply("Removed");
   }
 
   @Command("test")
@@ -92,24 +108,30 @@ text: ${lastItem.text}
 
   @Help()
   async help(ctx: Context) {
-    await ctx.replyWithMarkdown(
-      "RSS to Telegram bot v" +
-        process.env.npm_package_version +
-        "\n\nAfter successfully adding a RSS link, the bot starts fetching the feed every " +
-        delay +
-        " seconds. (This can be changed)" +
-        "\n\nTitles are used to easily manage RSS feeds and need to contain only one word" +
-        "\n\ncommands:" +
-        "\n/help Posts this help message" +
-        "\n/add title http://www.RSS-URL.com" +
-        "\n/remove !Title! removes the RSS link" +
-        "\n/list Lists all the titles and the RSS links from the DB" +
-        "\n/test Inbuilt command that fetches a post from Reddits RSS." +
-        "\n\nThe current chatId is: " +
-        "\n\nIf you like the project, ⭐ it on [DockerHub](https://hub.docker.com/r/bokker/rss.to.telegram) / [GitHub](https://www.github.com/BoKKeR/RSS-to-Telegram-Bot)",
-      {
-        disable_web_page_preview: true,
-      }
-    );
+    try {
+      await ctx.replyWithMarkdown(delay.toString());
+      await ctx.replyWithMarkdown(
+        "RSS to Telegram bot v" +
+          process.env.npm_package_version +
+          "\n\nAfter successfully adding a RSS link, the bot starts fetching the feed *every " +
+          delay +
+          " seconds*. (This can be changed)" +
+          "\n\nTitles are used to easily manage RSS feeds and need to contain only one word" +
+          "\n\ncommands:" +
+          "\n`/help` shows this help message" +
+          "\n`/add title http://www.RSS-URL.com`" +
+          "\n`/remove link_name` removes the RSS link, multiple links can be removed with one command" +
+          "\n`/list` Lists all the titles and the RSS links from the DB" +
+          "\n`/test` Inbuilt command that fetches a post from Reddits RSS." +
+          "\n\nThe current set chatId is: " +
+          chatid +
+          "\n\nIf you like the project, ⭐ it on [DockerHub](https://hub.docker.com/r/bokker/rss.to.telegram) / [GitHub](https://www.github.com/BoKKeR/RSS-to-Telegram-Bot)",
+        {
+          disable_web_page_preview: true,
+        }
+      );
+    } catch (error) {
+      await ctx.replyWithMarkdown("ERROR: " + error);
+    }
   }
 }
--- src/util/config.ts
+++ src/util/config.ts
@@ -1,3 +1,5 @@
 export const delay = parseInt(process.env.DELAY)
   ? parseInt(process.env.DELAY)
   : 120;
+
+export const chatid = process.env.CHATID;
