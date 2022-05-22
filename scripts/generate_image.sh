090243c05ea4486c9845251fe071f19214cdc852
Norbert Takacs <norbert.takacs@postnord.com>
2022-05-21 22:46:17 -0500

    updated scripts

--- scripts/generate_image.sh
+++ scripts/generate_image.sh
@@ -7,5 +7,5 @@ do
     IMAGE_PATH=$IMAGE_FOLDER/$IMAGE.md
     echo $IMAGE_PATH
     IMAGE_HASH=$(md5 -q $IMAGE_PATH)
-    ./node_modules/.bin/carbon-now --headless --config carbon-config.json $IMAGE_PATH -t $IMAGE -l img
+    ./node_modules/.bin/carbon-now --headless --config ./carbon-config.json $IMAGE_PATH -t $IMAGE -l img
 done
